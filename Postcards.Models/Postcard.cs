6f53b370eb35238da4b6f5d6adc39de11171e4e8
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2024-06-05 11:05:14 +0200

    add ef

---
+++ Postcards.Models/Postcard.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Postcards.Models;
+
+public class Postcard(string prompt, string userId)
+{
+    public int Id { get; private set; }
+    public string Prompt { get; private set; } = prompt;
+    public string UserId { get; private set; } = userId;
+    public DateTime CreatedAt { get; private set; } = DateTime.Now;
+    public string? ImageUrl { get; set; }
+    public DateTime? FulfillmentDate { get; set; }
+
+    private Postcard() : this( prompt:"", userId:"") // EF Core requires a parameterless constructor
+    {}
+}
---
+++ Postcards.gRPC/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Postcards.Models;
+
+namespace Postcards.gRPC.Data;
+
+public class AppDbContext(IConfiguration configuration) : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
+
+    public DbSet<Postcard> Postcards { get; set; }
+    
+}
