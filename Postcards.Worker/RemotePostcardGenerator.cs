282d210ec9ac11502fc46d3f7b17c53e381b3c03
Piotr-Niewczas <piotr.niewczas2002@gmail.com>
2024-06-12 22:10:27 +0200

    moved generator to Postcards.Generator.csproj

---
+++ Postcards.Worker/RemotePostcardGenerator.cs
@@ -0,0 +1,23 @@
+﻿using ErrorOr;
+
+namespace Postcards.Worker;
+
+public class RemotePostcardGenerator(string generatorServiceUrl, string baseImageHostUrl) : IPostcardGenerator
+{
+    public async Task<ErrorOr<string>> GeneratePostcard(string baseImgName, string text, int requestId)
+    {
+        var httpClient = new HttpClient();
+
+        try
+        {
+            var responseString = await httpClient.GetStringAsync(
+                $"{generatorServiceUrl}/generate?baseImgName={Uri.EscapeDataString(baseImgName)}&text={Uri.EscapeDataString(text)}&requestId={requestId}");
+            responseString = responseString.Trim('"');
+            return responseString;
+        }
+        catch (Exception e)
+        {
+            return Error.Unexpected(description: e.Message);
+        }
+    }
+}
